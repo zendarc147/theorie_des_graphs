cfd981c69e9f17a014dabd960eddebe45a0553c1
lilymad <lily.madsen@edu.ece.fr>
2025-12-07 15:39:07 +0100

    theme1 prob 1 hyp2 H02- commentaires

--- src/theme1/prob1/Hyp2/H02/Maison2.java
+++ src/theme1/prob1/Hyp2/H02/Maison2.java
@@ -2,17 +2,23 @@ package src.theme1.prob1.Hyp2.H02;
 
 public class Maison2 {
 
+    //sommet depart rue de la maison
     public int depart;
+    //sommet arrivee rue de la maison
     public int arrivee;
+    //true si maison du cote gauche, cote droit sinon
     public boolean coteG;
+    //true si non ramassable (ou deja ramasse)
     public boolean verif;
 
+    //constructeur
     public Maison2(int depart, int arrivee, boolean coteG) {
         this.depart = depart;
         this.arrivee = arrivee;
         this.coteG = coteG;
         this.verif = false;
     }
+    //aide chatgpt pour regler un probleme de String
     @Override
     public String toString() {
         return "(" + depart + "->" + arrivee + ", coteG = "+coteG+")";
